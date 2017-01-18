1e3ffd6cac69eae2b8f60f07ac79db309c89894d
v1kttor <viktoras@bmx.lt>
2017-01-18 20:24:07 +0000

    added line views/hrbirthday.xml

--- __manifest__.py
+++ __manifest__.py
@@ -25,6 +25,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        #'views/hrbirthday.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
