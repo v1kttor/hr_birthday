e558623efdb0509f8ab19fcd63c55dcbd33f06e5
Viktoras Norkus <viktoras@bmx.lt>
2017-01-18 21:43:33 +0200

    created hr_birthday.py and hr_department.py

---
+++ models/hr_department.py
@@ -0,0 +1,11 @@
+# Comments
+
+from odoo import fields, models, api
+
+class Department(models,Model):
+    _name = hr_birthday.department
+
+    check_birthdays = fields.Boolean(default=True)
+    birthday_remind_days = fields.Integer()
+
+    
