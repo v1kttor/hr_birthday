95647ebd63e7edcf4e0c20c9dc1ae30ff8f1c0ea
Viktoras Norkus <viktoras@bmx.lt>
2017-01-19 17:51:28 +0200

    minor changes

--- models/models.py
+++ models/models.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import timedelta
 
- class hr_birthday(models.Model):
-     _name = 'hr_birthday.hr_birthday'
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+class hrbirthday(models.Model):
+    _name = 'hr_birthday.hrbirthday'
 
-     @api.depends('value')
-     def _value_pc(self):
-         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
+
+
+class Birthday(models,Model):
+    _name = 'hrbirthday.birthday'
+
+    birthday_employee = fields.Many2one('hr.employee')
+    birthday_date = fields.Date()
+    department_id = fields.Many2one('hr.department')
+    celebration_date = fields.Datetime()
+    active = fields.Boolean(default=True)
+
+class Department(models,Model):
+    _name = 'hrbirthday.department'
+
+    check_birthdays = fields.Boolean(default=True)
+    birthday_remind_days = fields.Integer()
