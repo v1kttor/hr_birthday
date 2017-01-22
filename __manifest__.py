032cccff4331f276e9ea351438bf7f3bb3617889
Viktoras Norkus <viktoras@bmx.lt>
2017-01-22 17:50:00 +0200

    minor changes

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/hrbirthday.xml',
+        #'views/hrbirthday.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
