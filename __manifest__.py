343d7c78342e19f3a9208f54628c49abd9548d23
Viktoras Norkus <viktoras@bmx.lt>
2017-01-23 13:23:03 +0200

    minor changes

--- __manifest__.py
+++ __manifest__.py
@@ -5,11 +5,11 @@
     'summary': """Birthdays reminder""",
 
     'description': """
-        Long description of module's purpose
+        Very very very long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "hacby",
+    'website': "http://www.hacby.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
@@ -18,14 +18,16 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr'],
+    'depends': ['base', 'hr', 'mail'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        #'views/hrbirthday.xml',
+        'security/ir.model.access.csv', # nors dar reiktu patvarkyt pagal hive uzduoti . daugiau pagooglint
+        #'views/views.xml',
+        #'views/templates.xml',
+        'views/hrbirthday.xml',
+        'security/security.xml',
+        'views/reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
