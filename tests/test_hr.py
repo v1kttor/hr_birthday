182537e81995e3568dd653ba6a13da0d22f0a932
Viktoras Norkus <viktoras@bmx.lt>
2017-02-06 16:00:32 +0200

    tests added

---
+++ tests/test_hr.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo.tests import common
+from datetime import date
+
+
+class TestHrEmployee(common.TransactionCase):
+
+    def test_upcoming_birthday(self):
+        test_cases = [
+            (date(2017, 02, 15), '1987-02-18', 5, date(2017, 02, 18)),
+            (date(2017, 02, 15), '1987-03-18', 5, None),
+        ]
+        for i in test_cases:
+            today = i[0]
+            employee = self.env['hr.employee'].create({
+                'birthday': i[1],
+                'name': 'Jonas Sladkevičius'})
+            employee_birthday =  employee.get_upcoming_birthday_date(
+                i[2], today)
+            self.assertEqual(employee_birthday, i[3])
