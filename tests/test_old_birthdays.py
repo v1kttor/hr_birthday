796dd0208473fb5ac7c675fd0dc9bccb4c85e745
Viktoras Norkus <viktoras@bmx.lt>
2017-02-15 13:48:29 +0200

    updated tests/test_old_birthdays.py

--- tests/test_old_birthdays.py
+++ tests/test_old_birthdays.py
@@ -7,7 +7,7 @@ from datetime import date, timedelta
 class TestOldBirthdays(common.TransactionCase):
 
     def test_old_birthday_events(self):
-        test_casess = [
+        test_cases = [
             (date(2017, 5, 16), date(2017, 5, 2), 14, date(2017, 5, 2)),
             (date(2017, 2, 16), date(2017, 2, 3), 14, date(2017, 2, 2)),
             (date(2017, 3, 16), date(2017, 3, 1), 14, date(2017, 3, 2))
@@ -17,13 +17,12 @@ class TestOldBirthdays(common.TransactionCase):
             'birthday': '2017-02-17',
             'name': 'Jonas Sladkevičius'})
 
-        for i in test_casess:
+        for i in test_cases:
             today = i[0]
             event = self.env['hr.birthday'].create({
                 'birthday_date': i[1],
-                'birthday_employee': employee.id,
-                'active': True})
+                'birthday_employee': employee.id})
             old_event = event.get_old_birthdays(
                 i[2], today)
-            aa = i[0] - timedelta(days=i[2])
-            self.assertEqual(aa,  i[3])
+            diff = i[0] - timedelta(days=i[2])
+            self.assertEqual(diff,  i[3])
