64fce1c2a22e10bf6214cf141435a36ecc7a1f91
Viktoras Norkus <viktoras@bmx.lt>
2017-02-15 11:18:23 +0200

    updated __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,20 +2,21 @@
 {
     'name': "Birthdays",
 
-    'summary': """Birthdays module which remind
-     us about our colleagues upcoming birthday""",
-
-    'description': """
-
-    """,
-
+    'summary': """Birthdays module which reminds us about our colleagues
+    upcoming birthday. Module only informs us about our department
+    colleagues birthdays, so we will not know about other departments.
+    Also, module lets the user print report which will show all
+    department employees
+    birthdays. Also, there is a feature which marks birthday events
+    t false if they are older than two weeks.""",
+    'description': "",
     'author': "Viktoras",
     'website': "",
 
     # Categories can be used to filter modules in modules listing
     # for the full list
     'category': 'HR',
-    'version': '0.1',
+    'version': '10.0.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr', 'mail'],
@@ -29,9 +30,5 @@
         'data/optional.xml',
         'views/hr.xml',
         'views/mail.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
