ec444f940587521fbe8ea5665c1a06d193567e31
Viktoras Norkus <viktoras@bmx.lt>
2017-02-15 14:28:08 +0200

    update

---
+++ README.rst
@@ -0,0 +1,7 @@
+Birthdays module which reminds us about our colleagues upcoming birthday.
+Module only informs us about our department colleagues birthdays,
+ so we will not know about other departments.
+A module lets the user to print reports which indicates all the
+upcoming birthday.
+birthdays. Also, there is a feature which marks birthday events
+t false if they are older than two weeks.
--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Birthdays",
+    'name': "HR Birthday Reminder",
     'summary': """Birthdays module which reminds us about our colleagues
-    upcoming birthday. Module only informs us about our department
-    colleagues birthdays, so we will not know about other departments.
-    A module lets the user to print reports which indicates all the
-    upcoming birthday.
-    birthdays. Also, there is a feature which marks birthday events
-    t false if they are older than two weeks.""",
+    upcoming birthday.""",
     'description': "Birthday reminder",
     'author': "Viktoras",
     'website': "",
