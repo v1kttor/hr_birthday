9a70a2e6291cf5f88470b2e49bdbf844efbccf7b
Viktoras Norkus <viktoras@versada.eu>
2017-06-01 20:28:50 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,19 +5,9 @@
     upcoming birthday.""",
     'description': "Birthday reminder",
     'author': "Viktoras",
-    'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # for the full list
     'category': 'HR',
     'version': '10.0.0.1.0',
-
-    # any module necessary for this one to work correctly
-    'depends': [
-        'hr',
-     ],
-
-    # always loaded
+    'depends': ['hr'],
     'data': [
         'security/ir.model.access.csv',
         'security/hr_birthday.xml',
